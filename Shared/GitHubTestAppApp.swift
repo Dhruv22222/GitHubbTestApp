e37c1223e4dc574643056e94218af09202655055
Dhruv22222 <guptadhruv2008@gmail.com>
2021-02-24 13:40:51 -0800

    Initial Commit

---
+++ Shared/GitHubTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestAppApp.swift
+//  Shared
+//
+//  Created by Gupta, Manish on 2/24/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
